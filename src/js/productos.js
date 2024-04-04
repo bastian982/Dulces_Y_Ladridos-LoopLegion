fce92973df64764151538bd421cd83bdee03c5b3
Frida Estrada <fridaestrada24@gmail.com>
2024-04-04 00:04:55 -0600

    Productos responsive

--- src/js/productos.js
+++ src/js/productos.js
@@ -14,27 +14,28 @@ const getProducts = () => {
   );  
 }
 
-const cards = (productos) =>
-  productos.map(
-    (data) => `
-<div class="card col-md-4 m-4" >
-<img src="../img/default.png" class="card-img-top mt-2" alt="...">
-<div class="card-body">
-  <h6 class="card-title">${data.name}</h6>
-  <p class="card-text">$ ${data.price}</p>
-  <div class="text-end">
-    <button class="btnProduct"> <img src="../img/carrito.png" alt=""> </button>
-  </div> 
-</div>
-</div>
-    `
-  );
+const cards = (productos) => {
+  return productos.map((producto, index) => `
+    <div class="card col-md-4 col-6 m-4 ${index % 2 === 0 ? 'cardNaranja' : 'cardCafe'}" >
+      <img src="../img/default.png" class="card-img-top mt-2" alt="...">
+      <div class="card-body">
+        <h6 class="card-title">${producto.name}</h6>
+        <p class="card-text">$ ${producto.price}</p>
+        <div class="text-end">
+          <button class="btnProduct"> <img src="../img/carrito.png" alt=""> </button>
+        </div> 
+      </div>
+    </div>
+  `);
+};
+
 /* <a href="./publicacion_lectura.html" class="btn btn-primary">Ver mas . . .</a> */
 const printCards = (container, cards) =>
   (document.getElementById(container).innerHTML = cards.join(""));
 
 const printAll = async (url) => {
   let data = await getProducts(url);
+  console.log(data);
   printCards("cards", cards(data));
 };
 
