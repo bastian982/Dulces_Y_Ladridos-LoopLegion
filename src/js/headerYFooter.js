68c6fdbd8b522894d69e237fe94578147fa39450
Frida Estrada <fridaestrada24@gmail.com>
2024-04-11 14:38:46 -0600

    Funcionalidad carrito terminada

--- src/js/headerYFooter.js
+++ src/js/headerYFooter.js
@@ -27,7 +27,7 @@ document.getElementById("main-header").innerHTML = `
       </form>
       <div>
         <a href="#"><i class="fa-regular fa-user"></i></a>
-        <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+        <a href="./carrito.html"><i class="fa-solid fa-cart-shopping"></i></a>
       </div>
     </div>
 
